c13abb0a20c59f31963bbdf7c0498029c93fc3dd
Christian Petrov <cpetrov@eclipsesource.com>
2019-03-26 16:12:29 +0100

    Prevent Cordova from being installed in unexpected location
    When running `npm install`, npm tries to find a parent directory, which
    contains either a `node_modules` folder or `package.json` and installs
    the module relative to them [1]. This is a problem, since if a user has
    `node_modules` folder in his home directory, the Cordova CLI will be
    installed in `~/node_modules` instead of
    `~/.tabris-cli/cordova/${version}/node_modules`.
    
    To prevent that, create a valid package.json in the Cordova cache
    directory, so that Cordova will always be installed in the expected
    directory.
    
    [1]: https://github.com/npm/npm/issues/2896#issuecomment-32741508
    
    Change-Id: I54b219f242c64acbc8d293ee21547299a0007f31

--- src/services/CordovaCliInstaller.js
+++ src/services/CordovaCliInstaller.js
@@ -1,6 +1,7 @@
-const {mkdirsSync, existsSync} = require('fs-extra');
+const {mkdirsSync, existsSync, writeJsonSync} = require('fs-extra');
 const proc = require('../helpers/proc');
 const {sep} = require('path');
+const packageJson = require('../../package.json');
 
 module.exports = class CordovaCliInstaller {
 
@@ -15,6 +16,10 @@ module.exports = class CordovaCliInstaller {
       return installedCordovaPath;
     }
     mkdirsSync(`${this._cliDataDir}${sep}cordova${sep}${version}`);
+    writeJsonSync(`${this._cliDataDir}${sep}cordova${sep}${version}${sep}package.json`, {
+      name: `tabris-cli-cordova-${version}-cache`,
+      version: packageJson.version
+    });
     let {status} = proc.execSync('npm', ['install', `cordova@${version}`], {cwd: cordovaDir});
     if (status !== 0) {
       throw new Error('Error installing Cordova CLI.');
--- test/CordovaCliInstaller.test.js
+++ test/CordovaCliInstaller.test.js
@@ -1,9 +1,10 @@
-const {realpathSync, mkdirsSync} = require('fs-extra');
+const {realpathSync, mkdirsSync, readJsonSync} = require('fs-extra');
 const temp = require('temp');
 const {sep} = require('path');
 const proc = require('../src/helpers/proc');
 const CordovaCliInstaller = require('../src/services/CordovaCliInstaller');
 const {expect, restore, stub} = require('./test');
+const {version} = require('../package.json');
 
 describe('CordovaCliInstaller', function() {
 
@@ -32,6 +33,21 @@ describe('CordovaCliInstaller', function() {
 
       let path = installer.install('6.5.0');
       expect(path).to.equal(`${dir}${sep}cordova${sep}6.5.0${sep}node_modules${sep}.bin${sep}cordova`);
+    });
+
+    it('creates package.json', function() {
+      let dir = realpathSync(temp.mkdirSync('foo'));
+
+      stub(proc, 'execSync')
+        .withArgs('npm', ['install', 'cordova@6.5.0'], {cwd: `${dir}${sep}cordova${sep}6.5.0`})
+        .returns({status: 0});
+
+      let installer = new CordovaCliInstaller(dir);
+
+      let path = installer.install('6.5.0');
+      let packageJson = readJsonSync(`${dir}${sep}cordova${sep}6.5.0${sep}package.json`);
+      expect(path).to.equal(`${dir}${sep}cordova${sep}6.5.0${sep}node_modules${sep}.bin${sep}cordova`);
+      expect(packageJson).to.deep.equal({name: 'tabris-cli-cordova-6.5.0-cache', version});
     });
 
     it('throws an error if npm process returns non-0 exit code', function() {
