a236c3db8f702f74f154b2c7bcff25a06db6f229
Christian Petrov <cpetrov@eclipsesource.com>
2020-03-06 15:36:35 +0100

    Simplify AppReloader tests
    Remove unneeded describe() wrappers that only contained a single test
    case.
    
    It is not necessary to write the getfiles output to foo.js when testing
    the AppReloader. This only made tests harder to understand.
    
    Wait a few milliseconds before writing the file on macOS, since writing
    it right after registering the file for watching resulted in no change
    events being triggered.
    
    Consistently configure test timeouts to 6s and remove redundant timeout
    configuration from individual test cases.
    
    Change-Id: I3c1b1897eff5119bf72d83f71a82b6e761910952

--- test/AppReloader.test.js
+++ test/AppReloader.test.js
@@ -1,4 +1,5 @@
 const temp = require('temp');
+const os = require('os');
 const fetch = require('node-fetch');
 const {join} = require('path');
 const Server = require('../src/services/Server');
@@ -11,7 +12,7 @@ describe('AppReloader', function() {
 
   let server, path, oldCwd, reloader;
 
-  this.timeout(10000);
+  this.timeout(6000);
 
   beforeEach(function() {
     path = realpathSync(temp.mkdirSync('foo'));
@@ -28,90 +29,65 @@ describe('AppReloader', function() {
     restore();
   });
 
-  describe('when changed file was never requested', function() {
-
-    it('does not send reload command', async function() {
-      writeTabrisProject(path);
-      await server.serve(path);
-      let response = await fetch(`http://127.0.0.1:${server.port}/foo.js`);
-      let text = await response.text();
-      spy(server.debugServer, 'evaluate');
-      writeFileSync(join(path, 'bar.js'), `${text};`);
-      return new Promise(resolve => {
-        setTimeout(() => {
-          expect(server.debugServer.evaluate).not.to.have.been.called;
-          resolve();
-        }, 1500);
-      });
-    }).timeout(6000);
-
+  it('does not send reload command when changed file was never requested', async function() {
+    writeTabrisProject(path);
+    await server.serve(path);
+    await fetch(`http://127.0.0.1:${server.port}/foo.js`);
+    spy(server.debugServer, 'evaluate');
+    await overwriteFileContent(join(path, 'bar.js'));
+    return new Promise(resolve => {
+      setTimeout(() => {
+        expect(server.debugServer.evaluate).not.to.have.been.called;
+        resolve();
+      }, 1500);
+    });
   });
 
-  describe('when changed file was requested', function() {
-
-    it('sends a reload command', async function() {
-      writeTabrisProject(path);
-      await server.serve(path);
-      let response = await fetch(`http://127.0.0.1:${server.port}/foo.js`);
-      let text = await response.text();
-      spy(server.debugServer, 'reloadApp');
-      writeFileSync(join(path, 'foo.js'), `${text};`);
-      await waitForCalls(server.debugServer.reloadApp);
-    }).timeout(6000);
-
+  it('sends a reload command when changed file was requested', async function() {
+    writeTabrisProject(path);
+    await server.serve(path);
+    await fetch(`http://127.0.0.1:${server.port}/foo.js`);
+    spy(server.debugServer, 'reloadApp');
+    await overwriteFileContent(join(path, 'foo.js'));
+    await waitForCalls(server.debugServer.reloadApp);
   });
 
-  describe('when changed source file is in requested directory', function() {
-
-    it('sends a reload command', async function() {
-      mkdirSync(join(path, 'bar'));
-      writeTabrisProject(path, '{"main": "bar/foo.js"}');
-      writeFileSync(join(path, 'bar', 'foo.js'), 'console.log("test")');
-      writeFileSync(join(path, 'bar', 'baz.js'), 'console.log("test")');
-      await server.serve(path);
-      let response = await fetch(`http://127.0.0.1:${server.port}/bar?getfiles=${encodeURIComponent('*')}`);
-      let text = await response.text();
-      spy(server.debugServer, 'reloadApp');
-      writeFileSync(join(path, 'bar', 'baz.js'), `${text};`);
-      await waitForCalls(server.debugServer.reloadApp);
-    }).timeout(6000);
-
+  it('sends a reload command when changed source file is in requested directory', async function() {
+    mkdirSync(join(path, 'bar'));
+    writeTabrisProject(path, '{"main": "bar/foo.js"}');
+    await overwriteFileContent(join(path, 'bar', 'foo.js'));
+    await overwriteFileContent(join(path, 'bar', 'baz.js'));
+    await server.serve(path);
+    await fetch(`http://127.0.0.1:${server.port}/bar?getfiles=${encodeURIComponent('*')}`);
+    spy(server.debugServer, 'reloadApp');
+    await overwriteFileContent(join(path, 'bar', 'baz.js'));
+    await waitForCalls(server.debugServer.reloadApp);
   });
 
-  describe('when changed source file is in requested project root directory', function() {
-
-    it('sends a reload command', async function() {
-      writeTabrisProject(path);
-      await server.serve(path);
-      let response = await fetch(`http://127.0.0.1:${server.port}/package.json?getfiles=${encodeURIComponent('*')}`);
-      let text = await response.text();
-      spy(server.debugServer, 'reloadApp');
-      writeFileSync(join(path, 'foo.js'), `${text};`);
-      await waitForCalls(server.debugServer.reloadApp);
-    }).timeout(6000);
-
+  it('sends a reload command when changed source file is in requested project root directory', async function() {
+    writeTabrisProject(path);
+    await server.serve(path);
+    await fetch(`http://127.0.0.1:${server.port}/package.json?getfiles=${encodeURIComponent('*')}`);
+    spy(server.debugServer, 'reloadApp');
+    await overwriteFileContent(join(path, 'foo.js'));
+    await waitForCalls(server.debugServer.reloadApp);
   });
 
-  describe('when changed non-source file is in request directory', function() {
-
-    it('does not send reload command', async function() {
-      mkdirSync(join(path, 'bar'));
-      writeTabrisProject(path, '{"main": "bar/foo.js"}');
-      writeFileSync(join(path, 'bar', 'foo.js'), 'console.log("test")');
-      writeFileSync(join(path, 'bar', 'baz'), 'console.log("test")');
-      await server.serve(path);
-      let response = await fetch(`http://127.0.0.1:${server.port}/bar?getfiles=${encodeURIComponent('*')}`);
-      let text = await response.text();
-      spy(server.debugServer, 'evaluate');
-      writeFileSync(join(path, 'bar', 'baz'), `${text};`);
-      return new Promise(resolve => {
-        setTimeout(() => {
-          expect(server.debugServer.evaluate).not.to.have.been.called;
-          resolve();
-        }, 1500);
-      });
-    }).timeout(6000);
-
+  it('does not send reload command when changed non-source file is in request directory', async function() {
+    mkdirSync(join(path, 'bar'));
+    writeTabrisProject(path, '{"main": "bar/foo.js"}');
+    await overwriteFileContent(join(path, 'bar', 'foo.js'));
+    await overwriteFileContent(join(path, 'bar', 'baz'));
+    await server.serve(path);
+    await fetch(`http://127.0.0.1:${server.port}/bar?getfiles=${encodeURIComponent('*')}`);
+    spy(server.debugServer, 'evaluate');
+    await overwriteFileContent(join(path, 'bar', 'baz'));
+    return new Promise(resolve => {
+      setTimeout(() => {
+        expect(server.debugServer.evaluate).not.to.have.been.called;
+        resolve();
+      }, 1500);
+    });
   });
 
 });
@@ -139,4 +115,13 @@ function waitForCalls(spyInstance, minCallCount = 1) {
       }
     }, 100);
   });
+}
+
+const wait = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+async function overwriteFileContent(path) {
+  if (os.platform() === 'darwin') {
+    await wait(100);
+  }
+  writeFileSync(path, 'overwritten-content');
 }
