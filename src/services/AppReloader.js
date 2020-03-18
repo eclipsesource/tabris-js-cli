eclipsesource/tabris-js-cli

commit 7ff7f86c1069faaa1b4ce443709086f8808074e6

Reload app on reconnect
Ensure a reload command is executed when an app with the same session ID
reconnects.

Migrate DebugConnection callback functions to EventEmitter events to
reduce the overhead of maintaining two event mechanisms and to enable
multiple subscribers.

Remove AppReloader integration tests, since they were very brittle due
to dependence on internals. Replace them with unit tests that test the
AppReloader module alone and basic acceptance tests based on behavior
inspecting a real WebSocket connection.

Fix a bug in the reloader resulting in file watching not working when
the -p/--project flag is given to the CLI.

Remove unneeded passing of appPath parameters, it was available as a
property on the Server already. Make it public to make it available to
the AppReloader as well.

Change-Id: I7970c3ec62c8edfcbad567510fa5b25da10f962f

src/services/AppReloader.js
@@ -1,29 +1,42 @@
 const filewatcher = require('filewatcher');
+const {join} = require('path');
 
 module.exports = class AppReloader {
 
-  constructor(server) {
+  constructor(server, watcher = filewatcher({debounce: 100, persistent: true})) {
     this._server = server;
-    this._watcher = filewatcher({
-      debounce: 100,
-      persistent: true
-    });
-    this._server.on('deliver', url => {
-      this._watcher.add(url);
-    });
+    this._watcher = watcher;
+    this._lastSessionId = null;
+    this._server.on('deliver', url => this._watcher.add(join(server.appPath, url)));
+    this._server.debugServer.once('connect', id => this._lastSessionId = id);
+    this._reloadApp = (sessionId = this._lastSessionId) => {
+      if (this._lastSessionId !== sessionId) {
+        return;
+      }
+      const success = this._server.debugServer.reloadApp();
+      if (!success) {
+        this._server.debugServer.removeListener('connect', this._reloadApp);
+        this._server.debugServer.once('connect', this._reloadApp);
+      }
+      this._lastSessionId = sessionId;
+    };
   }
 
   start() {
     this._watcher.on('change', (filename, stat) => {
-      if (stat && this._server.debugServer.reloadApp()) {
+      if (stat && this._lastSessionId !== null) {
         this._server.terminal.info(`'${filename}' changed, reloading app...`);
+        this._reloadApp();
       }
     });
+    return this;
   }
 
-  // only using for unit testing
+
+  // only using for testing
   stop() {
     this._watcher.removeAll();
+    return this;
   }
 
 };

src/services/Server.js
@@ -61,15 +61,15 @@ module.exports = class Server extends EventEmitter {
     if (!appPath) {
       throw new Error('path missing');
     }
-    this._appPath = appPath;
+    this.appPath = appPath;
     this.serverId = join(appPath, (main || 'package.json')) + '#' + Date.now();
     let stats = await this._lstat(appPath);
     if (stats.isDirectory()) {
-      this._packageJson = this._readAppPackageJson(appPath, main);
-      this._tabrisVersion = parseInt(this._readTabrisPackageJson(appPath).version.split('.')[0], 10);
+      this._packageJson = this._readAppPackageJson(main);
+      this._tabrisVersion = parseInt(this._readTabrisPackageJson().version.split('.')[0], 10);
       this._checkTabrisVersion();
       this._runProjectScript();
-      await this._startServer(appPath, main);
+      await this._startServer(main);
     } else {
       throw new Error('Project must be a directory.');
     }
@@ -98,8 +98,8 @@ module.exports = class Server extends EventEmitter {
     }
   }
 
-  _readAppPackageJson(appPath, main) {
-    let packageJsonPath = join(appPath, 'package.json');
+  _readAppPackageJson(main) {
+    let packageJsonPath = join(this.appPath, 'package.json');
     if (!existsSync(packageJsonPath)) {
       throw new Error('Directory must contain package.json');
     }
@@ -113,8 +113,8 @@ module.exports = class Server extends EventEmitter {
     return content;
   }
 
-  _readTabrisPackageJson(appPath) {
-    let packageJsonPath = join(appPath, 'node_modules', 'tabris', 'package.json');
+  _readTabrisPackageJson() {
+    let packageJsonPath = join(this.appPath, 'node_modules', 'tabris', 'package.json');
     if (!existsSync(packageJsonPath)) {
       throw new Error('No tabris module installed; did you run npm install?');
     }
@@ -131,24 +131,24 @@ module.exports = class Server extends EventEmitter {
   }
 
   _runProjectScript() {
-    proc.spawnSync('npm', ['run', '--if-present', 'build'], {cwd: this._appPath});
+    proc.spawnSync('npm', ['run', '--if-present', 'build'], {cwd: this.appPath});
     if (this._watch) {
       const ps = proc.spawn(
         'npm',
         ['run', '--if-present', 'watch'],
-        {cwd: this._appPath, stdio: 'pipe'}
+        {cwd: this.appPath, stdio: 'pipe'}
       );
       ps.stdout.on('data', data => this.terminal.log(data.toString().trim()));
       ps.stderr.on('data', data => this.terminal.error(data.toString().trim()));
     }
   }
 
-  async _startServer(appPath, main) {
+  async _startServer(main) {
     if (!Server.externalAddresses.length) {
       throw new Error('No remotely accessible network interfaces');
     }
     const app = connect();
-    this._createMiddlewares(appPath, main).forEach(middleware => app.use(middleware));
+    this._createMiddlewares(this.appPath, main).forEach(middleware => app.use(middleware));
     let port = this._port || await this._findAvailablePort();
     return new Promise((resolve, reject) => {
       this._server = app.listen(port, err => {
@@ -173,16 +173,16 @@ module.exports = class Server extends EventEmitter {
     }
   }
 
-  _createMiddlewares(appPath, main) {
+  _createMiddlewares(main) {
     return [
       this._createErrorHandler(),
       this._createRequestEmitter(),
-      this._createGetFilesMiddleware(appPath),
+      this._createGetFilesMiddleware(),
       this._createDeliverEmitter(),
       this._createPackageJsonMiddleware(main),
-      this._createBootJsMiddleware(appPath),
+      this._createBootJsMiddleware(),
       this._createDefaultRouteMiddleware(),
-      serveStatic(appPath),
+      serveStatic(this.appPath),
       this._create404Handler()
     ];
   }
@@ -212,11 +212,11 @@ module.exports = class Server extends EventEmitter {
     };
   }
 
-  _createGetFilesMiddleware(appPath) {
+  _createGetFilesMiddleware() {
     const fileService = new FileService({
-      [join(appPath, 'package.json')]: JSON.stringify(this._packageJson)
+      [join(this.appPath, 'package.json')]: JSON.stringify(this._packageJson)
     });
-    const getFiles = new GetFilesMiddleware(appPath, fileService);
+    const getFiles = new GetFilesMiddleware(this.appPath, fileService);
     getFiles.on('deliver', url => this.emit('deliver', url));
     return getFiles.handleRequest.bind(getFiles);
   }
@@ -241,7 +241,7 @@ module.exports = class Server extends EventEmitter {
     };
   }
 
-  _createBootJsMiddleware(appPath) {
+  _createBootJsMiddleware() {
     if (this._tabrisVersion < 3) {
       return (req, res, next) => next();
     }
@@ -249,7 +249,7 @@ module.exports = class Server extends EventEmitter {
       if (req.url === '/node_modules/tabris/boot.min.js') {
         res.setHeader('content-type', 'text/plain');
         return res.end(getBootJs(
-          appPath,
+          this.appPath,
           this.debugServer.getNewSessionId(),
           encodeURIComponent(this.serverId)
         ));

test/AppReloader.test.js
@@ -1,0 +1,136 @@
+const {EventEmitter} = require('events');
+const {expect, stub} = require('./test');
+const AppReloader = require('../src/services/AppReloader');
+const TerminalMock = require('./TerminalMock');
+
+describe('AppReloader', function() {
+
+  let server;
+  let watcher;
+  let appReloader;
+  const SESSION_ID = 1;
+
+
+  beforeEach(() => {
+    server = new ServerMock();
+    watcher = new FileWatcherMock();
+    appReloader = new AppReloader(server, watcher);
+  });
+
+  it('tracks files on deliver', () => {
+    server.emit('deliver', 'file-path');
+
+    expect(watcher.add).to.have.been.calledWith('app-path/file-path');
+  });
+
+  describe('start', () => {
+
+    beforeEach(() => appReloader.start());
+
+    it('ignores file changes before first connection', () => {
+      watcher.emit('change', 'filename', 'stat');
+
+      expect(server.debugServer.reloadApp).not.to.have.been.called;
+      expect(server.terminal.info).not.to.have.been.called;
+    });
+
+    describe('after first connection', () => {
+
+      beforeEach(() => server.debugServer.emit('connect', SESSION_ID));
+
+      it('does not reload app when file stats not available ', () => {
+        watcher.emit('change', 'filename');
+
+        expect(server.debugServer.reloadApp).not.to.have.been.called;
+      });
+
+      it('reloads app when file stats available', () => {
+        watcher.emit('change', 'filename', 'stat');
+
+        expect(server.debugServer.reloadApp).to.have.been.calledOnce;
+      });
+
+      it('prints info', () => {
+        watcher.emit('change', 'filename', 'stat');
+
+        expect(server.terminal.info).to.have.been.calledWithMatch(/'filename' changed/);
+      });
+
+      describe('when reload fails', () => {
+
+        beforeEach(() => server.debugServer.reloadApp.returns(false));
+
+        it('reloads on reconnect', () => {
+          watcher.emit('change', 'filename', 'stat');
+
+          server.debugServer.reloadApp.reset();
+          server.debugServer.reloadApp.returns(true);
+          server.debugServer.emit('connect', SESSION_ID);
+
+          expect(server.debugServer.reloadApp).to.have.been.calledOnce;
+        });
+
+        it('does not reload on reconnect if connected with a new session ID', () => {
+          watcher.emit('change', 'filename', 'stat');
+
+          server.debugServer.reloadApp.reset();
+          server.debugServer.reloadApp.returns(true);
+          server.debugServer.emit('connect', SESSION_ID + 1);
+
+          expect(server.debugServer.reloadApp).not.to.have.been.called;
+        });
+
+        it('does not reload more than once when reconnecting multiple times', () => {
+          watcher.emit('change', 'filename', 'stat');
+
+          server.debugServer.reloadApp.reset();
+          server.debugServer.reloadApp.returns(true);
+          server.debugServer.emit('connect', SESSION_ID);
+          server.debugServer.emit('connect', SESSION_ID);
+          server.debugServer.emit('connect', SESSION_ID);
+
+          expect(server.debugServer.reloadApp).to.have.been.calledOnce;
+        });
+
+        it('reloads only once on reconnect when files changed multiple times while the connection was lost', () => {
+          watcher.emit('change', 'filename', 'stat');
+          watcher.emit('change', 'filename', 'stat');
+          watcher.emit('change', 'filename', 'stat');
+
+          server.debugServer.reloadApp.reset();
+          server.debugServer.reloadApp.returns(true);
+          server.debugServer.emit('connect', SESSION_ID);
+
+          expect(server.debugServer.reloadApp).to.have.been.calledOnce;
+        });
+
+      });
+
+    });
+
+  });
+
+});
+
+class ServerMock extends EventEmitter {
+  constructor() {
+    super();
+    this.terminal = new TerminalMock();
+    this.debugServer = new DebugServerMock();
+    this.appPath = 'app-path';
+  }
+}
+
+class FileWatcherMock extends EventEmitter {
+  constructor() {
+    super();
+    this.add = stub();
+  }
+}
+
+class DebugServerMock extends EventEmitter {
+  constructor() {
+    super();
+    this.reloadApp = stub();
+  }
+}

test/serve.acceptance.test.js
@@ -1,10 +1,11 @@
 const {join, resolve} = require('path');
 const {writeFileSync, realpathSync} = require('fs-extra');
 const temp = require('temp');
-const {expect, restore, writeTabrisProject} = require('./test');
+const {stub, expect, restore, writeTabrisProject} = require('./test');
 const spawn = require('child_process').spawn;
 const fetch = require('node-fetch');
 const {platform} = require('os');
+const WebSocket = require('ws');
 
 // General note: On windows "spawn" can be unexpectedly slow.
 // Therefore waitForStdout gets very long timeout, and as consequence
@@ -12,7 +13,7 @@ const {platform} = require('os');
 
 describe('serve', function() {
 
-  let serve, path, env;
+  let serve, path, env, webSocketClient;
   const mockBinDir = join(__dirname, 'bin');
 
   beforeEach(function() {
@@ -25,6 +26,9 @@ describe('serve', function() {
   afterEach(() => {
     if (serve) {
       serve.kill();
+    }
+    if (webSocketClient) {
+      webSocketClient.close();
     }
     serve = null;
     restore();
@@ -139,6 +143,52 @@ describe('serve', function() {
     let data = await response.json();
     expect(data.main).to.equal('foo.js');
   }).timeout(8000);
+
+  it('does not reload client when a changed file was never loaded', async function() {
+    writeTabrisProject(path);
+    serve = spawn('node', ['./src/tabris', 'serve', '-wap', path], {env});
+    const stdout = await waitForStdout(serve);
+    const port = getPortFromStdout(stdout);
+    webSocketClient = await startFakeWebSocketClient(port);
+    webSocketClient.onmessage = stub();
+
+    writeFileSync(join(path, 'foo.js'), 'content');
+
+    await waitForStdout(serve);
+    expect(webSocketClient.onmessage).not.to.have.been.called;
+  }).timeout(16000);
+
+  it('reloads client on file change', async function() {
+    writeTabrisProject(path);
+    serve = spawn('node', ['./src/tabris', 'serve', '-wap', path], {env});
+    const stdout = await waitForStdout(serve);
+    const port = getPortFromStdout(stdout);
+    webSocketClient = await startFakeWebSocketClient(port);
+    webSocketClient.onmessage = stub();
+    await fetch(`http://127.0.0.1:${port}/foo.js`);
+
+    writeFileSync(join(path, 'foo.js'), 'content');
+
+    const res = await waitForStdout(serve);
+    const {data} = webSocketClient.onmessage.getCall(0).args[0];
+    expect(res).to.contain('foo.js\' changed, reloading app...');
+    expect(JSON.parse(data)).to.deep.equal({'type': 'reload-app'});
+  }).timeout(16000);
+
+  it('does not reload client when the changed file was not a source file', async function() {
+    writeTabrisProject(path);
+    writeFileSync(join(path, 'bar'));
+    serve = spawn('node', ['./src/tabris', 'serve', '-wap', path], {env});
+    const stdout = await waitForStdout(serve);
+    const port = getPortFromStdout(stdout);
+    webSocketClient = await startFakeWebSocketClient(port);
+    webSocketClient.onmessage = stub();
+    await fetch(`http://127.0.0.1:${port}/bar`);
+
+    writeFileSync(join(path, 'bar'), 'content');
+
+    expect(webSocketClient.onmessage).not.to.have.been.called;
+  }).timeout(16000);
 
   it('starts a server on a directory given with --project', async function() {
     writeTabrisProject(path);
@@ -291,4 +341,17 @@ function getPortFromStdout(stdout) {
   let ports = stdout.match(/.*http:.*:(\d+).*/);
   expect(ports).to.be.a('array', 'No ports found in stdout: ' + stdout);
   return ports[1];
+}
+
+async function startFakeWebSocketClient(port) {
+  const bootMinJs = await (await fetch(`http://127.0.0.1:${port}/node_modules/tabris/boot.min.js`)).text();
+  const sessionId = bootMinJs.match(/sessionId: '(.*)',/m)[1];
+  const serverId = bootMinJs.match(/serverId: '(.*)',/m)[1];
+  return new Promise(resolve => {
+    const ws = new WebSocket(`ws://127.0.0.1:${port}/?session=${sessionId}&server=${serverId}`, '');
+    ws.onopen = () => {
+      ws.send(JSON.stringify({type: 'connect', parameter: {platform: 'foo', model: 'bar'}}));
+      resolve(ws);
+    };
+  });
 }

src/helpers/DebugConnection.js
@@ -1,14 +1,17 @@
+const {EventEmitter} = require('events');
+
 const STATE_CHECK_INTERVAL = process.env.NODE_ENV === 'test' ? 500 : 5000;
 const TYPE_CONNECTION = 'connect';
 const TYPE_LOG = 'log';
 const TYPE_ACTION_RESPONSE = 'action-response';
 
-module.exports = class DebugConnection {
+module.exports = class DebugConnection extends EventEmitter {
 
   /**
    * @param {string} address
    */
   constructor(address) {
+    super();
     this._sessionId = -1;
     this.address = address;
     this.device = {
@@ -49,22 +52,6 @@ module.exports = class DebugConnection {
     return false;
   }
 
-  set onConnect(cb) {
-    this._onConnect = cb;
-  }
-
-  set onDisconnect(cb) {
-    this._onDisconnect = cb;
-  }
-
-  set onLog(cb) {
-    this._onLog = cb;
-  }
-
-  set onActionResponse(cb) {
-    this._onActionResponse = cb;
-  }
-
   get sessionId() {
     return this._sessionId;
   }
@@ -75,17 +62,11 @@ module.exports = class DebugConnection {
         const clientMessage = JSON.parse(message);
         if (clientMessage.type === TYPE_CONNECTION) {
           this.device = clientMessage.parameter;
-          if (this._onConnect) {
-            this._onConnect(this);
-          }
+          this.emit('connect');
         } else if (clientMessage.type === TYPE_LOG) {
-          if (this._onLog) {
-            this._onLog(this, clientMessage.parameter);
-          }
+          this.emit('log', clientMessage.parameter);
         } else if(clientMessage.type === TYPE_ACTION_RESPONSE) {
-          if (this._onActionResponse) {
-            this._onActionResponse(this, clientMessage.parameter);
-          }
+          this.emit('actionResponse', clientMessage.parameter);
         }
       } catch (ex) {}
     });
@@ -115,9 +96,7 @@ module.exports = class DebugConnection {
 
   _closeWebSocket(code) {
     this._stopConnectionChecks();
-    if (this._onDisconnect) {
-      this._onDisconnect(this);
-    }
+    this.emit('disconnect');
     // prevent the 'close' listener from calling this method, causing a stack overflow
     const oldSocket = this._webSocket;
     this._webSocket = null;

src/services/DebugServer.js
@@ -1,4 +1,5 @@
 const DebugConnection = require('../helpers/DebugConnection');
+const {EventEmitter} = require('events');
 const url = require('url');
 
 const OUTDATED_CONNECTION_CLOSURE = 4900;
@@ -9,7 +10,7 @@ const messageTypes = Object.freeze({
   reloadApp: 'reload-app'
 });
 
-module.exports = class DebugServer {
+module.exports = class DebugServer extends EventEmitter {
 
   /**
    * @param {import('ws').Server} webSocketServer
@@ -17,6 +18,7 @@ module.exports = class DebugServer {
    * @param {string} serverId
    */
   constructor(webSocketServer, terminal, serverId) {
+    super();
     this._webSocketServer = webSocketServer;
     this._sessionCounter = 0;
     this._serverId = serverId;
@@ -95,16 +97,17 @@ module.exports = class DebugServer {
     return this._webSocketServer.options.port;
   }
 
-  _onConnect(connection) {
+  _handleConnect(connection, sessionId) {
     if (this._printStateTimer !== -1) {
       clearTimeout(this._printStateTimer);
       this._printStateTimer = -1;
     } else {
       this._printClientState(connection.device, STATE_CONNECTED);
     }
+    this.emit('connect', sessionId);
   }
 
-  _onDisconnect(connection) {
+  _handleDisconnect(connection) {
     if (this._printStateTimer !== -1) {
       clearTimeout(this._printStateTimer);
     }
@@ -117,13 +120,13 @@ module.exports = class DebugServer {
     }, this._printStateDelay);
   }
 
-  _onLog(connection, {messages}) {
+  _handleLog(_connection, {messages}) {
     for (const message of messages) {
       this._printClientMessage(message);
     }
   }
 
-  _onActionResponse(connection, {enablePrompt}) {
+  _handleActionResponse(_connection, {enablePrompt}) {
     if (enablePrompt && this._onEvaluationCompleted) {
       this._onEvaluationCompleted();
     }
@@ -167,10 +170,10 @@ Keyboard shortcuts:
 
   _createConnection(webSocket, sessionId, address) {
     const connection = new DebugConnection(address);
-    connection.onConnect = this._onConnect.bind(this);
-    connection.onDisconnect = this._onDisconnect.bind(this);
-    connection.onLog = this._onLog.bind(this);
-    connection.onActionResponse = this._onActionResponse.bind(this);
+    connection.on('connect', () => this._handleConnect(connection, sessionId));
+    connection.on('disconnect', () => this._handleDisconnect(connection));
+    connection.on('log', args => this._handleLog(connection, args));
+    connection.on('actionResponse', args => this._handleActionResponse(connection, args));
     connection.open(webSocket, sessionId);
     return connection;
   }

test/AppReloader.integration.test.js
@@ -1,102 +1,0 @@
-const temp = require('temp');
-const os = require('os');
-const fetch = require('node-fetch');
-const {join} = require('path');
-const Server = require('../src/services/Server');
-const AppReloader = require('../src/services/AppReloader');
-const {expect, restore, spy, waitForCalls, writeTabrisProject} = require('./test');
-const {writeFileSync, realpathSync, mkdirSync} = require('fs-extra');
-const TerminalMock = require('./TerminalMock.js');
-
-describe('AppReloader', function() {
-
-  let server, path, oldCwd, reloader;
-
-  this.timeout(6000);
-
-  beforeEach(function() {
-    path = realpathSync(temp.mkdirSync('foo'));
-    oldCwd = process.cwd();
-    process.chdir(path);
-    server = new Server({terminal: new TerminalMock()});
-    reloader = new AppReloader(server);
-    reloader.start();
-  });
-
-  afterEach(function() {
-    process.chdir(oldCwd);
-    reloader.stop();
-    restore();
-  });
-
-  it('does not send reload command when changed file was never requested', async function() {
-    writeTabrisProject(path);
-    await server.serve(path);
-    await fetch(`http://127.0.0.1:${server.port}/foo.js`);
-    spy(server.debugServer, 'evaluate');
-    await overwriteFileContent(join(path, 'bar.js'));
-    return new Promise(resolve => {
-      setTimeout(() => {
-        expect(server.debugServer.evaluate).not.to.have.been.called;
-        resolve();
-      }, 1500);
-    });
-  });
-
-  it('sends a reload command when changed file was requested', async function() {
-    writeTabrisProject(path);
-    await server.serve(path);
-    await fetch(`http://127.0.0.1:${server.port}/foo.js`);
-    spy(server.debugServer, 'reloadApp');
-    await overwriteFileContent(join(path, 'foo.js'));
-    await waitForCalls(server.debugServer.reloadApp);
-  });
-
-  it('sends a reload command when changed source file is in requested directory', async function() {
-    mkdirSync(join(path, 'bar'));
-    writeTabrisProject(path, '{"main": "bar/foo.js"}');
-    await overwriteFileContent(join(path, 'bar', 'foo.js'));
-    await overwriteFileContent(join(path, 'bar', 'baz.js'));
-    await server.serve(path);
-    await fetch(`http://127.0.0.1:${server.port}/bar?getfiles=${encodeURIComponent('*')}`);
-    spy(server.debugServer, 'reloadApp');
-    await overwriteFileContent(join(path, 'bar', 'baz.js'));
-    await waitForCalls(server.debugServer.reloadApp);
-  });
-
-  it('sends a reload command when changed source file is in requested project root directory', async function() {
-    writeTabrisProject(path);
-    await server.serve(path);
-    await fetch(`http://127.0.0.1:${server.port}/package.json?getfiles=${encodeURIComponent('*')}`);
-    spy(server.debugServer, 'reloadApp');
-    await overwriteFileContent(join(path, 'foo.js'));
-    await waitForCalls(server.debugServer.reloadApp);
-  });
-
-  it('does not send reload command when changed non-source file is in request directory', async function() {
-    mkdirSync(join(path, 'bar'));
-    writeTabrisProject(path, '{"main": "bar/foo.js"}');
-    await overwriteFileContent(join(path, 'bar', 'foo.js'));
-    await overwriteFileContent(join(path, 'bar', 'baz'));
-    await server.serve(path);
-    await fetch(`http://127.0.0.1:${server.port}/bar?getfiles=${encodeURIComponent('*')}`);
-    spy(server.debugServer, 'evaluate');
-    await overwriteFileContent(join(path, 'bar', 'baz'));
-    return new Promise(resolve => {
-      setTimeout(() => {
-        expect(server.debugServer.evaluate).not.to.have.been.called;
-        resolve();
-      }, 1500);
-    });
-  });
-
-});
-
-const wait = (ms) => new Promise(resolve => setTimeout(resolve, ms));
-
-async function overwriteFileContent(path) {
-  if (os.platform() === 'darwin') {
-    await wait(100);
-  }
-  writeFileSync(path, 'overwritten-content');
-}

test/test.js
@@ -13,6 +13,7 @@ let sandbox = sinon.sandbox.create();
 let spy = sandbox.spy.bind(sandbox);
 let stub = sandbox.stub.bind(sandbox);
 let match = sinon.match;
+let createStubInstance = sinon.createStubInstance;
 
 function restore() {
   sandbox.restore();
@@ -36,6 +37,7 @@ function writeTabrisProject(path, projectPackage, tabrisPackage) {
     mkdirSync(join(path, 'node_modules'), {recursive: true});
     mkdirSync(join(path, 'node_modules', 'tabris'), {recursive: true});
     writeFileSync(join(path, 'node_modules', 'tabris', 'package.json'), tabrisPackage || '{"version": "3.0.0"}');
+    writeFileSync(join(path, 'node_modules', 'tabris', 'boot.min.js'));
   }
 }
 
@@ -64,4 +66,4 @@ function waitForCalls(spyInstance, minCallCount = 1, maxDelay = 1500) {
   });
 }
 
-module.exports = {expect, spy, stub, match, restore, writeTabrisProject, waitForCalls};
+module.exports = {expect, spy, stub, createStubInstance, match, restore, writeTabrisProject, waitForCalls};

commit 5fb5441b74116738ab980ef9078be528d82ef9b5

Fix app reloading
App reloading was fixed for non-preloaded files but was unexpectedly
broken for preloaded files in 7ff7f86.

The file watcher expects full file paths. GetFilesMiddleware already
produces full file paths. Fix the deliver event for non-preloaded files
to emit full file paths as well.

Change-Id: If69d295797fff7be5748d7c6fa52d14bd6bc2c69

src/services/AppReloader.js
@@ -1,5 +1,4 @@
 const filewatcher = require('filewatcher');
-const {join} = require('path');
 
 module.exports = class AppReloader {
 
@@ -7,7 +6,7 @@ module.exports = class AppReloader {
     this._server = server;
     this._watcher = watcher;
     this._lastSessionId = null;
-    this._server.on('deliver', url => this._watcher.add(join(server.appPath, url)));
+    this._server.on('deliver', url => this._watcher.add(url));
     this._server.debugServer.once('connect', id => this._lastSessionId = id);
     this._reloadApp = (sessionId = this._lastSessionId) => {
       if (this._lastSessionId !== sessionId) {

src/services/Server.js
@@ -1,5 +1,5 @@
 const os = require('os');
-const {join} = require('path');
+const {posix, join} = require('path');
 const EventEmitter = require('events');
 const {readJsonSync, existsSync, lstat} = require('fs-extra');
 const serveStatic = require('serve-static');
@@ -234,7 +234,7 @@ module.exports = class Server extends EventEmitter {
 
   _createDeliverEmitter() {
     return (req, res, next) => {
-      this.emit('deliver', req.url.slice(1));
+      this.emit('deliver', posix.join(this.appPath, req.url));
       next();
     };
   }

test/AppReloader.test.js
@@ -20,7 +20,7 @@ describe('AppReloader', function() {
   it('tracks files on deliver', () => {
     server.emit('deliver', 'file-path');
 
-    expect(watcher.add).to.have.been.calledWith('app-path/file-path');
+    expect(watcher.add).to.have.been.calledWith('file-path');
   });
 
   describe('start', () => {

test/serve.acceptance.test.js
@@ -157,7 +157,25 @@ describe('serve', function() {
     expect(webSocketClient.onmessage).not.to.have.been.called;
   }).timeout(16000);
 
-  it('reloads client on file change', async function() {
+  it('reloads client when a preloaded file changes', async function() {
+    writeTabrisProject(path);
+    serve = spawn('node', ['./src/tabris', 'serve', '-wap', path], {env});
+    const stdout = await waitForStdout(serve);
+    const port = getPortFromStdout(stdout);
+    webSocketClient = await startFakeWebSocketClient(port);
+    webSocketClient.onmessage = stub();
+    await fetch(`http://127.0.0.1:${port}/package.json?getfiles=${encodeURIComponent('*')}`);
+    await wait(500);
+
+    writeFileSync(join(path, 'foo.js'), 'content');
+
+    const res = await waitForStdout(serve);
+    const {data} = webSocketClient.onmessage.getCall(0).args[0];
+    expect(res).to.contain('foo.js\' changed, reloading app...');
+    expect(JSON.parse(data)).to.deep.equal({'type': 'reload-app'});
+  }).timeout(16000);
+
+  it('reloads client when a non-preloaded file changes', async function() {
     writeTabrisProject(path);
     serve = spawn('node', ['./src/tabris', 'serve', '-wap', path], {env});
     const stdout = await waitForStdout(serve);
@@ -334,4 +352,8 @@ async function startFakeWebSocketClient(port) {
       resolve(ws);
     };
   });
+}
+
+function wait(time) {
+  return new Promise(resolve => setTimeout(resolve), time);
 }